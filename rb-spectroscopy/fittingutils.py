eacd10bd4f80c7ec97e0ae890dbd8b84c521c268
andres-luengo <andres.luengo@yale.edu>
2026-04-09 16:22:27 -0400

    lorentzian

--- rb-spectroscopy/fittingutils.py
+++ rb-spectroscopy/fittingutils.py
@@ -1,5 +1,15 @@
 import numpy as np
 
+"""
+if you want a proper gaussian, amplitude = 1/sqrt(2pi * sigma**2), baseline = 0
+"""
 def gaussian(x, center, sigma, amplitude, baseline):
     exp = -0.5*((x - center)/sigma)**2
     return baseline + amplitude * np.exp(exp)
+
+"""
+if you want a proper lorentzian, amplitude = 1/(pi * gamma), baseline = 0
+"""
+def lorentzian(x, center, gamma, amplitude, baseline):
+    denom = 1 + ((x - center) / gamma)**2
+    return baseline + amplitude / denom
